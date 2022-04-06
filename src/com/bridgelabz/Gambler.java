e5f6c84d79f7c894b2b27e8c3608bdf61cbac82a
HemnathG <hemnathdavid@gmail.com>
2022-04-06 21:03:23 +0530

    50% amount after the game completion Added

--- src/com/bridgelabz/Gambler.java
+++ src/com/bridgelabz/Gambler.java
@@ -6,11 +6,17 @@ public class Gambler {
     int amount = 100;
 
     public static void main(String[] args) {
-        int bet = (int) (Math.floor(Math.random() * 10) % 2);
-        if (bet == 1) {
-            System.out.println("WON the Game");
-        } else {
-            System.out.println("LOOSE the Game");
+        int stake =100;
+        while(stake>50 && stake <150) {
+            int bet = (int) (Math.floor(Math.random() * 10) % 2);
+            if (bet == 1) {
+                System.out.println("WON the Game");
+                stake++;
+            } else {
+                System.out.println("LOOSE the Game");
+                stake--;
+            }
         }
+        System.out.println("Amount After the game :" +stake);
     }
 }
